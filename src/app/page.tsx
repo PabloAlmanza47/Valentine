eb5be4ffba81d518d3e5007b304148d1bdddff04
Mistical47 <pabloalmanza3247@gmail.com>
2026-02-02 10:07:08 -0600

    Adding a glabal css color and basic pop out box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
+'use client'; //telling next to use the client side to render the page
+
 import Image from "next/image";
+import React, { useState } from 'react';
+
 
 export default function Home() {
+  const [isExpanded, setIsExpanded] = useState(false)
   return (
-    <div className="bg-[#FF8896] h-screen w-full flex flex-col items-center justify-center"> 
-      <div className="bg-[#FFBBC1] text-black">
-        Testing
-      </div>
-    </div>
+    <main className="bg-background h-screen w-full flex flex-col items-center justify-center"> 
+      {!isExpanded ? (
+        <div onClick={() => setIsExpanded(true)} className="bg-boxColor rounded-xl p-8 cursor-pointer hover:shadow-xl transition-shadow">
+          <p className="font-mono text-center">
+            You have mail!
+          </p>
+          <p className="font-mono text-center text-xs">
+            click me
+          </p>
+        </div>
+      ) : (
+        <div onClick={() => setIsExpanded(false)} className="bg-boxColor rounded-xl shadow-lg p-8 cursor-pointer">
+          Box 2
+        </div>
+      )}
+
+
+
+
+
+    </main>
   );
 }
